df4daa433b785dc050ac92d5f603f89f7da4da46
unknown <sharadasapkota409@gmail.com>
2025-04-09 15:53:05 +0545

    first commit

---
+++ Javascript/javascript.js
@@ -0,0 +1,36 @@
+subtraction 
+mutiplication
+division
+addition
+
+
+
+
+
+var num1=10;
+var num2=20;
+var sum = num1 + num2;
+console.log(sum);
+console.log('the sum of ${num1} and ${num2} is ${sum}');
+
+
+var jodayako=num1+num2;
+var ghatako=num1-num2;
+var gudan=num1*num2;
+var vaga=num1/num2;
+
+
+
+
+
+console.log(jodayako);
+console.log(ghatako);
+console.log(gudan);
+console.log(vaga);
+
+
+
+var num1="10";
+var num2=20;
+var sum = num1 + num2;
+console.log(sum);
